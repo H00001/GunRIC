37a68ae745602e3a2383c9f7f4e52c6f71f48221
dosdrtt <lnpj.office@icloud.com>
2019-06-29 21:14:44 +0800

    init 4.1.5.7 beta 29 update 34 pom.xml

--- GunRIC-Common/src/top/gunplan/ric/common/GunRicStdFilter.java
+++ GunRIC-Common/src/top/gunplan/ric/common/GunRicStdFilter.java
@@ -7,7 +7,6 @@ import top.gunplan.netty.impl.GunNettyOutputFilterChecker;
 import top.gunplan.ric.protocol.AbstractGunRicProtocol;
 import top.gunplan.ric.protocol.GunRicTypeDividePacketManage;
 import top.gunplan.utils.AbstractGunBaseLogUtil;
-
 import static top.gunplan.netty.GunNettyFilter.DealResult.NEXT;
 import static top.gunplan.netty.GunNettyFilter.DealResult.NOTDEALALLNEXT;
 
@@ -21,18 +20,12 @@ public class GunRicStdFilter implements GunNettyFilter {
 
     @Override
     public DealResult doInputFilter(GunNettyInputFilterChecker gunInputFilterChecker) {
-        try {
-
-            AbstractGunRicProtocol protocol = GunRicTypeDividePacketManage.findPackage(gunInputFilterChecker.getSrc());
-            if (protocol.unSerialize(gunInputFilterChecker.getSrc())) {
-                gunInputFilterChecker.setObject(protocol);
-                return NEXT;
-            } else {
-                AbstractGunBaseLogUtil.urgency("protocol unSerialize fail", getClass().getName());
-                return NOTDEALALLNEXT;
-            }
-        } catch (Exception exp) {
-            AbstractGunBaseLogUtil.urgency("unknown exception fail", exp.getMessage());
+        AbstractGunRicProtocol protocol = GunRicTypeDividePacketManage.findPackage(gunInputFilterChecker.getSrc());
+        if (protocol.unSerialize(gunInputFilterChecker.getSrc())) {
+            gunInputFilterChecker.setTransfer(protocol);
+            return NEXT;
+        } else {
+            AbstractGunBaseLogUtil.urgency("protocol unSerialize fail", getClass().getName());
             return NOTDEALALLNEXT;
         }
     }
