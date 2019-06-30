2f9c84e44f61082da4f80eead9aa6318e72765be
frank <renda.han@gaea.com>
2019-06-30 09:27:02 +0800

    change user struct update 2

---
+++ GunRIC-User/src/java/top/gunplan/ric/user/GunRicUserException.java
@@ -0,0 +1,10 @@
+package top.gunplan.ric.user;
+
+import top.gunplan.ric.protocol.exp.GunRicException;
+
+/**
+ * GunRicUserException
+ *
+ */
+public class GunRicUserException extends GunRicException {
+}
